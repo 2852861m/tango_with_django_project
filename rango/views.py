d0084a6136b611ed5e178d973046fdfb44702900
Sean McBride <2852861m@student.gla.ac.uk>
2025-02-06 21:47:03 +0000

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to client
-    # First parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Try to find a category
+        category = Category.objects.get(slug=category_name_slug)
+        
+        # Retrieve the associated pages
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    
+    # Render the response and return it to the client
+    return render(request, 'rango/category.html', context=context_dict)
